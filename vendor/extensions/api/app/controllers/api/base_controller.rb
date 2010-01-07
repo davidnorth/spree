886b688e568157368098559f2564fe1a920e8cb4
David North <davidnorth@railsdog.com>
2010-01-07 22:01:32 +0000

    translations for event error messages

--- vendor/extensions/api/app/controllers/api/base_controller.rb
+++ vendor/extensions/api/app/controllers/api/base_controller.rb
@@ -11,14 +11,14 @@ class Api::BaseController < Spree::BaseController
     valid_events_for_object = object.state_transitions.map(&:event)
 
     if params[:e].blank?
-      errors = 'No event specified'
+      errors = t('api.errors.missing_event')
     elsif valid_events_for_object.include?(params[:e].to_sym)
       object.send("#{params[:e]}!")
       errors = nil
     elsif valid_events.include?(params[:e].to_sym)
-      errors = "Valid event name but not allowed for this object, allowed events are #{valid_events_for_object.to_sentence}"
+      errors = t('api.errors.invalid_event_for_object', :events => valid_events_for_object.join(','))
     else
-      errors = "Invalid event name, valid event names are #{valid_events.to_sentence}"
+      errors = t('api.errors.invalid_event', :events => valid_events.join(','))
     end
 
     respond_to do |wants|
@@ -26,6 +26,7 @@ class Api::BaseController < Spree::BaseController
         if errors.blank?
           render :nothing => true
         else
+          puts errors
           render :json => errors.to_json, :status => 422
         end
       end
@@ -33,3 +34,4 @@ class Api::BaseController < Spree::BaseController
   end
 
 end
+
