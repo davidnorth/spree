98d0f5e86b8822bb9e96fe93e10ec469fe6c20f9
David North <davidnorth@railsdog.com>
2010-01-11 10:42:47 +0000

    remove uncessary actions from api routes

--- vendor/extensions/api/config/routes.rb
+++ vendor/extensions/api/config/routes.rb
@@ -2,9 +2,9 @@ map.namespace :admin do |admin|
   admin.resources :users, :member => {:generate_api_key => :put, :clear_api_key => :put}
 end
 map.namespace :api do |api|
-  api.resources :shipments, :member => {:event => :put}
-  api.resources :orders, :member => {:event => :put} do |orders|
-    orders.resources :shipments
-    orders.resources :line_items
+  api.resources :shipments, :except => [:new,:edit], :member => {:event => :put}
+  api.resources :orders, :except => [:new,:edit], :member => {:event => :put} do |orders|
+    orders.resources :shipments, :except => [:new,:edit]
+    orders.resources :line_items, :except => [:new,:edit]
   end
 end
