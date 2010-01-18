aa06aded28a03d05b10e7fdc5f9c8c6d2a5c3230
David North <davidnorth@railsdog.com>
2010-01-18 11:30:20 +0000

    implement basic refund action, add controller test

--- app/controllers/admin/creditcards_controller.rb
+++ app/controllers/admin/creditcards_controller.rb
@@ -3,4 +3,11 @@ class Admin::CreditcardsController < Admin::BaseController
   belongs_to :order
   actions :index
   
+  def refund
+    load_object
+    @creditcard_txn = @creditcard.creditcard_txns.find(params[:txn_id])
+    @creditcard.credit(params[:amount].to_f, @creditcard_txn)
+    redirect_to collection_path
+  end
+  
 end
