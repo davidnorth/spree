dae314592ab550cf6bcf43aa1811343af2763bed
David North <davidnorth@railsdog.com>
2010-02-25 11:49:29 +0000

    working on ajax

--- vendor/extensions/theme_default/public/javascripts/spree.js
+++ vendor/extensions/theme_default/public/javascripts/spree.js
@@ -25,9 +25,49 @@ jQuery.fn.radioControlsVisibilityOfElement = function(dependentElementSelector){
   showValue = this.get(0).value;
   radioGroup = $("input[name='" + this.get(0).name + "']");
   radioGroup.each(function(){
-    $(this).click(function(){
-      $(dependentElementSelector).visible(this.checked && this.value == showValue)
+    jQuery(this).click(function(){
+      jQuery(dependentElementSelector).visible(this.checked && this.value == showValue)
     });
     if(this.checked){ this.click() }
   });
 }
+
+jQuery.fn.ajaxForm = function(){
+  this.submit(function(){
+    //$('#' + $(this).attr("target") + '_spinner').show();
+    jQuery.post(jQuery(this).attr("action"), jQuery(this).serialize(), null, "script")
+    return false;
+  })
+}
+
+var request = function(options) {
+  jQuery.ajax(jQuery.extend({ url : options.url, type : 'get' }, options));
+  return false;
+};
+ 
+// remote links handler
+jQuery('a[data-remote=true]').live('click', function() {
+  alert('remote link');
+  if(method = jQuery(this).attr("data-method")){
+    return request({ url: this.href, type: 'POST', data: {'_method': method} });
+  } else {
+    return request({ url: this.href });
+  }
+});
+ 
+// remote forms handler
+jQuery('form[data-remote=true]').live('submit', function() {
+  return request({ url : this.action, type : this.method, data : jQuery(this).serialize() });
+});
+
+
+//$('.ajax_delete_link').unbind('click').click(function(){
+//  if(confirm('Are you sure?')){
+//    $('#'+$(this).attr("target")+'_spinner').show();
+//    $.post(this.href, {'_method': 'delete'}, null, 'script');
+//  }
+//  return false;
+//})
+
+
+
