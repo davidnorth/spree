1d59ec7113b88e751c099bf24411a88eae115d77
David North <davidnorth@railsdog.com>
2011-04-18 22:46:20 +0100

    PromotionRule subclass eligible should accept options arg

--- promo/app/models/promotion/rules/first_order.rb
+++ promo/app/models/promotion/rules/first_order.rb
@@ -1,6 +1,6 @@
 class Promotion::Rules::FirstOrder < PromotionRule
 
-  def eligible?(order)
+  def eligible?(order, options = {})
     order.user && order.user.orders.checkout_complete.count == 0
   end
 
